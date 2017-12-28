a7b823e2b82170302bd12c2e1547b8fc07442c49
baesparza <bruno.be81@gmail.com>
2017-12-28 15:33:10 -0500

    feat(tree): add Preorder

---
+++ code/data_structures/src/tree/tree/operations/traversing/Preorder.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+struct Node {
+	Node(int v) : value(v), left(nullptr), right(nullptr) {}
+	Node(int v, Node * l, Node * r) : value(v), left(l), right(r) {}
+	int value;
+	Node * left;
+	Node * right;
+};
+
+void printPreorder(Node * tree) {
+	if (!tree) return;
+	std::cout << tree->value << ", ";
+	printPreorder(tree->left);
+	printPreorder(tree->right);
+}
+
+int main() {
+	/*
+		  4
+		 / \
+		1   3
+	   /   / \
+	 -2   1   2
+	   \     / \
+		3  -2  -3
+	*/
+	Node * root = new Node(
+		4,
+		new Node(
+			1,
+			new Node(
+				-2,
+				nullptr,
+				new Node(3)
+			), nullptr
+		),
+		new Node(
+			3,
+			new Node(1),
+			new Node(
+				2,
+				new Node(-2),
+				new Node(-3)
+			)
+		)
+	);
+
+	printPreorder(root);
+
+	system("pause");
+	return 0;
+}
